3c464427285832825a926236f6829daf111e9bba
Teddy <dudetotheleft@gmail.com>
2023-12-12 14:53:12 -0800

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/NewNoteFragment.kt
@@ -0,0 +1,4 @@
+package com.example.notesapp
+
+class NewNoteFragment {
+}
---
+++ app/src/main/java/com/example/notesapp/Note.kt
@@ -0,0 +1,9 @@
+package com.example.notesapp
+
+import android.os.Parcelable
+
+data class Note(
+    var title: String,
+    var content: String,
+    var date: String
+)
